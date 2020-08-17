76887d514a20adeec90c7321933c9c5a171c7a8f
Pat Downer <patricia.downer@gmail.com>
2020-08-16 23:20:23 -0700

    all prompts working, nothing else yet

--- app.js
+++ app.js
@@ -4,84 +4,84 @@ const path = require("path");
 
 const render = require("./lib/htmlRenderer");
 
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 let employees = []
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Write code to use inquirer to gather information about the development team members and to create objects for each team member(using the correct classes as blueprints!)
 
-// const buildManager = employee => {
-//   prompt([
-//     {
-//       type: 'number',
-//       name: 'officeNumber',
-//       message: 'Manager office number:'
-//     }
-//   ])
-//     .then(({ officeNumber }) => {
-//       employees.push(new Manager(employee.name, employee.id, employee.email, officeNumber))
-//       subMenu()
-//     })
-//     .catch(err => console.log(err))
-// }
+const buildManager = employee => {
+  prompt([
+    {
+      type: 'number',
+      name: 'officeNumber',
+      message: 'Manager office number:'
+    }
+  ])
+    .then(({ officeNumber }) => {
+      employees.push(new Manager(employee.name, employee.id, employee.email, officeNumber))
+      subMenu()
+    })
+    .catch(err => console.log(err))
+}
 
-// const buildEngineer = employee => {
-//   prompt([
-//     {
-//       type: 'input',
-//       name: 'github',
-//       message: 'Engineer GitHub username:'
-//     }
-//   ])
-//     .then(({ github }) => {
-//       employees.push(new Engineer(employee.name, employee.id, employee.email, github))
-//       subMenu()
-//     })
-//     .catch(err => console.log(err))
-// }
+const buildEngineer = employee => {
+  prompt([
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Engineer GitHub username:'
+    }
+  ])
+    .then(({ github }) => {
+      employees.push(new Engineer(employee.name, employee.id, employee.email, github))
+      subMenu()
+    })
+    .catch(err => console.log(err))
+}
 
-// const buildIntern = employee => {
-//   prompt([
-//     {
-//       type: 'number',
-//       name: 'school',
-//       message: 'Intern office number:'
-//     }
-//   ])
-//     .then(({ school }) => {
-//       employees.push(new Intern(employee.name, employee.id, employee.email, school))
-//       subMenu()
-//     })
-//     .catch(err => console.log(err))
-// }
+const buildIntern = employee => {
+  prompt([
+    {
+      type: 'input',
+      name: 'school',
+      message: 'Intern school name:'
+    }
+  ])
+    .then(({ school }) => {
+      employees.push(new Intern(employee.name, employee.id, employee.email, school))
+      subMenu()
+    })
+    .catch(err => console.log(err))
+}
 
-// const subMenu = () => {
-//   prompt({
-//     type: 'list',
-//     name: 'action',
-//     choices: ['Add Another Employee', 'Finish'],
-//     message: 'What would you like to do now?'
-//   })
-//     .then(({ action }) => {
-//       switch (action) {
-//         case 'Add Another Employee':
-//           mainMenu()
-//           break
-//         case 'Finish':
-//           const html = render(employees)
-//           fs.writeFileSync(path.join(__dirname, 'output', 'index.html'), html)
-//           break
-//       }
-//     })
-//     .catch(err => console.log(err))
-// }
+const subMenu = () => {
+  prompt({
+    type: 'list',
+    name: 'action',
+    choices: ['Add Another Employee', 'Finish'],
+    message: 'What would you like to do now?'
+  })
+    .then(({ action }) => {
+      switch (action) {
+        case 'Add Another Employee':
+          mainMenu()
+          break
+        case 'Finish':
+          const html = render(employees)
+          // fs.writeFileSync(path.join(__dirname, 'output', 'index.html'), html)
+          break
+      }
+    })
+    .catch(err => console.log(err))
+}
 
 const mainMenu = () => {
   prompt([
@@ -126,7 +126,7 @@ const mainMenu = () => {
           break
       }
     })
-    .catch(err = console.log(err))
+    .catch(err => console.log(err))
 }
 
 mainMenu()
